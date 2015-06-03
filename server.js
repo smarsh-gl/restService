ba800780a6bc06c8340fcb00f1148666d610bd06
Steve Marsh <smarsh@greenlancer.co>
2015-06-03 13:16:30 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,51 @@
+var Hapi = require("hapi");
+
+
+var server = new Hapi.Server();
+
+
+server.connect({
+	port:8888
+});
+
+
+
+server.route({
+	method:"GET",
+	path:"/array1",
+	handler: function(request, reply) {
+		var ret = [
+			{"name":"Steve"},
+			{"name":"Fred"}
+		];
+		reply(ret);
+	}
+});
+server.route({
+	method:"GET",
+	path:"/array2",
+	handler: function(request, reply) {
+		var ret = [
+			{"name":"Joe"},
+			{"name":"Earl"}
+		];
+		reply(ret);
+	}
+});
+server.route({
+	method:"GET",
+	path:"/array3",
+	handler: function(request, reply) {
+		var ret = [
+			{"name":"Frank"},
+			{"name":"Julie"}
+		];
+		reply(ret);
+	}
+});
+
+
+
+server.start(function() {
+	console.log("Server started");
+})
